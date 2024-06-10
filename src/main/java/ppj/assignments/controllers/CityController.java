5c3f935bf3a2162805f50c9f81e7ee276e259d02
Jan Podávka <72079041+JanPodavka@users.noreply.github.com>
2024-06-10 06:26:53 +0200

    Final version

--- src/main/java/ppj/assignments/controllers/CityController.java
+++ src/main/java/ppj/assignments/controllers/CityController.java
@@ -3,9 +3,7 @@ package ppj.assignments.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ppj.assignments.data.City;
-import ppj.assignments.data.Country;
 import ppj.assignments.service.CityService;
-import ppj.assignments.service.CountryService;
 
 import java.util.List;
 
--- src/main/java/ppj/assignments/repositories/CityRepository.java
+++ src/main/java/ppj/assignments/repositories/CityRepository.java
@@ -1,10 +1,8 @@
 package ppj.assignments.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import ppj.assignments.data.City;
-import ppj.assignments.data.Country;
 
 @Repository
 public interface CityRepository extends JpaRepository<City, Long> {
